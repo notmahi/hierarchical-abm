464ad2fb3adfe009a2417f3db42b03c67fc9bacc
Mahi Shafiullah <n.m.mahi@gmail.com>
2020-04-18 21:02:20 +0000

    Added a sample environment class for family

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ model.py contains the generic units of our agent based model, namely the people
 units and the social units.
 """
 
+import uuid
+import numpy as np
+
 from mesa import Agent, Model
 from constants import *
 
@@ -22,6 +25,8 @@ class Person(Agent):
                                 directly a part of.
     state (constants.state):    Which state of the disease is this person on 
                                 right now?
+    UID (str):                  Each agent is given a single UID to make 
+                                identification and location easy.
     """
     def __init__(self, gender, age, earning, location, model, state=None):
         self.gender = gender
@@ -34,7 +39,15 @@ class Person(Agent):
         self.state = state or COVID_state.S
         # To simplify computation, this is the hierarchy tree this model is a
         # part of
-        self.hierarchy_tree = 
+        self.hierarchy_tree = [self.model]
+        env_now = self.model
+        while model_now.superenv is not None:
+            # Build out the whole hierarchy so we can tell where this particular
+            # agent is located
+            self.hierarchy_tree.append(model_now.superenv)
+            model_now = model_now.superenv
+
+        self.uid = uuid.uuid4()
 
     def step(self):
         """ What does this agent do in one day? """
@@ -66,6 +79,8 @@ class EnvironmentModel(Model):
     GISmap (GIS object):            For visualization, the GIS object for this 
                                     environment. We will decide on particulars
                                     later.
+    visits (List):                  Environments are representative of physical
+                                    spaces, and thus we 
     """
     def __init__(subenvs, superenvs, population, area, GISmap):
         self.subenvs = subenvs
@@ -75,6 +90,7 @@ class EnvironmentModel(Model):
         self.GISmap = None # Will have to decide how to handle later
 
         self.pop_density = self.population / self.area
+        self.visits = []
 
     def step(self):
         """ 
@@ -84,9 +100,59 @@ class EnvironmentModel(Model):
         for subenv in self.subenvs:
             subenv.step()
         self.own_step()
+        self.clean_up_contacts()
+
+    def register_contact(self, agent):
+        """
+        To keep track of all the people who are visiting this environment 
+        through the day, we use this function that lets the agent attach its
+        uid to this particular environment
+        """
+        self.visits.append(agent.uid)
+
+    @property
+    def leaving_probability(self):
+        """
+        We want leaving probability to be dynamic, possibly, which is why we keep
+        it flexible.
+        """
+        raise NotImplementedError('You must define subenvironment steps.')
+
+    def clean_up_contacts(self):
+        """
+        Helper function to (probabilisitically) clean up the visits from the day
+        """
+        # Keep someone already in the env with probability 1-leaving_probability
+        self.visits = [x for x in self.visits if np.random.random_sample() > self.leaving_probability]
 
     def own_step(self):
+        """
+        We take this step once the steps at a subenv level has been completed.
+        We can think of this event as "what happened in the district" on a day
+        or other predefined timestep.
+        """
         raise NotImplementedError('You must define subenvironment steps.')
 
 
 
+class FamilyEnv(EnvironmentModel):
+    """
+    Modeling a family based on our hierarchical agent based model setup. 
+    Families or households are the lowest rung on our hierarchy, which are 
+    composed of person agents.
+    We used an average household area of 50 sq meters
+    """
+    def __init__(people, superenv, area=5.0e-5):
+        self.population = len(people)
+        self.people = people
+        self.superenv = superenv
+        self.area = area
+        super().__init__(people, superenv, population, area, None)
+
+    @property
+    def leaving_probability(self):
+        # You don't leave your family
+        return 0.0
+
+    def own_step(self):
+        pass
